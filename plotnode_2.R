f2d40c062170dc865535d8a8a86222a31a641177
camrinbraun <camrin.braun@gmail.com>
2018-06-01 07:00:39 -0400

    changed how handle colors as gg_color_hue led to confusion

--- plotnode_2.R
+++ plotnode_2.R
@@ -14,10 +14,12 @@
 
 plotNode <- function(inputFiles, node, plot=TRUE, plotLayout=NULL, returnData=FALSE, plotTrees=FALSE){
   
-  gg_color_hue <- function(n) {
-    hues = seq(15, 375, length = n + 1)
-    hcl(h = hues, l = 65, c = 100)[1:n]
-  }
+  #gg_color_hue <- function(n) {
+  #  hues = seq(15, 375, length = n + 1)
+  #  hcl(h = hues, l = 65, c = 100)[1:n]
+  #}
+  
+  pal <- rep(brewer.pal(5, 'Set1'), 4)
   
   fileList <- as.list(inputFiles)
   print(paste('Reading', length(fileList), 'input files and building trees...'))
@@ -54,7 +56,7 @@ plotNode <- function(inputFiles, node, plot=TRUE, plotLayout=NULL, returnData=FA
     
     par(mfrow = plotLayout)
     print(paste('colors are specified in the following order as:'))
-    print(paste(sapply(gg_color_hue(b), plotrix::color.id)))
+    print(paste(sapply(pal[1:b], plotrix::color.id)))
     print(paste('and match the following input files'))
     print(paste(fileList))
     
@@ -78,7 +80,8 @@ plotNode <- function(inputFiles, node, plot=TRUE, plotLayout=NULL, returnData=FA
         }
         
         # then the lines
-        lines(newDens[[b]], col=gg_color_hue(b)[b], lwd=1.5, lty=ceiling(b / 5))
+        #lines(newDens[[b]], col=gg_color_hue(length(saveNodes))[b], lwd=1.5, lty=ceiling(b / 5))
+        lines(newDens[[b]], col=pal[b], lwd=1.5, lty=ceiling(b / 5))
         
       } # end iteration through each file on individual plot
       
@@ -91,13 +94,55 @@ plotNode <- function(inputFiles, node, plot=TRUE, plotLayout=NULL, returnData=FA
 }
 
 # the directory where your file(s) live
-#setwd('~/Documents/WHOI/RCode/GruenDS/datedist/')
-setwd("/Users/dgruen/Desktop/clusterfucklocal/datedists_2018_05_02")
+setwd('~/Documents/WHOI/RCode/GruenDS/datedist/')
+#setwd("/Users/dgruen/Desktop/clusterfucklocal/datedists_2018_05_02")
 
 # get list of all files in your current directory
 fileList <- list.files()
 # make a vector of all files in your working directory whose filename contains 'datedist'
 fileList <- fileList[grep('datedist', fileList)]
 
-saveNodes <- plotNode(inputFiles=fileList, node=c(104,105,106,108,110), plotLayout=c(2,1), returnData = TRUE, plotTrees = F)
+saveNodes <- plotNode(inputFiles=fileList, node=c(104,105,106,108), plotLayout=c(2,2), returnData = TRUE, plotTrees = F)
+
+#Plot1:
+#  File: outfile_pb_molclock_18_05_01_ugam_sample.datedist
+#Nodes: 151, 176, 178, 208
+
+#Plot2:
+#  File: outfile_pb_molclock_18_05_02_ln_sample.datedist
+#Nodes: 151, 176, 178, 208
+
+pdf('plot_nodes_2May2018.pdf', height=12, width=8, useDingbats = F)
+node <- c(151,176,178,208)
+par(mfrow=c(2,1))
+for (b in c(1,3)){ # for files 1 and 3 in fileList
+  # starting density plots before redo density call with common bandwidth
+  #startDens <- lapply(saveNodes, FUN=function(x) density(x[,node[tt]]))
+  #xlims <- rev(range(unlist(lapply(newDens, FUN=function(x) range(x$x)))))
+  #if (b == 1){
+    xlims <- c(750,100)
+    ylims <- c(0,.015)
+  #}
+  
+ # if (b == 3){
+ #   xlims <- c(750,350)
+ #   ylims <- c(0,.015)
+ # }
+  
+  plot(0, 0, xlim = xlims, ylim = ylims, type='n', xlab='Years (Ma)', ylab='Density')
+  title(paste(fileList[[b]]))
+  legend('topleft', as.character(node), col=gg_color_hue(length(node)), lty=1)
+  
+  for (tt in 1:length(node)){
+    # get metrics to specify plot
+    old.bw <- unlist(lapply(startDens, FUN=function(x) x$bw))
+    new.bw <- diff(range(old.bw)) / 2 + min(old.bw)
+    newDens <- lapply(saveNodes, FUN=function(x) density(x[,node[tt]], bw=new.bw))
+    
+    # then the lines
+    lines(newDens[[b]], col=gg_color_hue(length(node))[tt], lwd=1.5, lty=ceiling(b / 5))
+  }
+}
+dev.off()
+
 
